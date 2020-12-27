908908b5c5f8638d62d49ab9d47720d5a446d2bd
Riska Abdullah <alternative.xen@gmail.com>
2020-12-27 18:20:49 +0800

    Optional Parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -15,15 +16,26 @@ app = FastAPI(
     version="0.0.1",
 )
 
+fake_items_db = [
+    {'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name': 'Baz'}
+]
+
+
+@app.get('/items/')
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: str, q: Optional[str] = None):
+    if q:
+        return {'item_id': item_id, 'q': q}
+    return {'item_id': item_id}
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
 
-
-@app.get('/items/{item_id}')
-async def read_item(item_id: int):
-    return {'item_id': item_id}
 
 
 @app.get('/users/me')
