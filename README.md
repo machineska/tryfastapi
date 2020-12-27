01b9328a86b43d774c90a3771ecf356704d196a4
Riska Abdullah <alternative.xen@gmail.com>
2020-12-27 17:51:09 +0800

    path parameters

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# tryfastapi
+# Try FastApi
+
--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(
+    title="Try FastAPI",
+    description="This is a very fancy project, with auto docs for the API and everything",
+    version="0.0.1",
+)
 
 
 @app.get('/')
 async def root():
     return {'message': 'Hello World'}
+
+
+@app.get('/items/{item_id}')
+async def read_item(item_id: int):
+    return {'item_id': item_id}
+
+
+@app.get('/users/me')
+async def read_user_me():
+    return {'user_id': 'the current user'}
+
+
+@app.get('/users/{user_id}')
+async def read_user(user_id: str):
+    return {'user_id': user_id}
